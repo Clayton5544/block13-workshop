98fbbeae7cdbb0ac2e1bd7364df3d6efca4fb0ad
Clayton5544 <142748623+Clayton5544@users.noreply.github.com>
2023-10-26 03:54:05 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const convertToCelsius = (fahrenheit) => {
+    return (fahrenheit) -32 * .5555556;
+    prompt(`Enter a number and I will convert it to celsius`);
+}
+  //prompt(`Enter a number and I will convert it to celsius`);
+
+  console.log(fahrenheit(90));
+
+
+
+  const createMessage = (fahren, celc) => {
+
+  }
+
+  const rand = (limit) => {
+
+  }
